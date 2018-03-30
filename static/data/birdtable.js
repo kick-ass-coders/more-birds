b9da2cb470098ce0be430a92b0e4dea026ec4595
ars0107 <arwen.ricard@gmail.com>
2018-03-29 20:55:58 -0500

    changed default number of rows added to 25

--- static/data/birdtable.js
+++ static/data/birdtable.js
@@ -1,3 +1,82 @@
+// // Get the input field
+// var input = document.getElementById("birdInput");
+
+// // Execute a function when the user releases a key on the keyboard
+// input.addEventListener("keyup", function(event) {
+//   // Cancel the default action, if needed
+//   event.preventDefault();
+//   // Number 13 is the "Enter" key on the keyboard
+//   if (event.keyCode === 13) {
+//     // Trigger the button element with a click
+//     document.getElementById("birdSearchBtn").click();
+//   }
+// });
+
+// add a click event here to get the input filter value
+
+var DataController = {
+  raw: [],
+  filters: {
+    textIncludes: function(search) {
+      return function(datum) {
+        return datum.toLowerCase().includes(search.toLowerCase())
+      }
+    }
+  },
+  filtersApplied: [],
+  filtered: [],
+  applyFilters: function() {
+    var allRejectedRows = []
+    for (const {column, filterName, value} of this.filtersApplied) {
+      console.log(this.columnarFormat)
+      const theseRejected = this.columnarFormat[column].map((v, i) => {
+        return [v, i]
+      }).filter(([v, i]) => {
+        return !this.filters[filterName](value)(v)
+      }).map(([v, i]) => {
+        return i 
+      })
+      allRejectedRows = allRejectedRows.concat(theseRejected)
+    }
+    this.filtered = this.raw.filter(function(datum, i) {
+      return !allRejectedRows.includes(i)
+    })
+  },
+  columnarFormat: {},
+  loadData: function(data) {
+    this.columnarFormat = {}
+    this.raw = data
+    return data.reduce(function(a, x) {
+      Object.entries(x).forEach(function([k, v]) {
+        if (k in a) {
+          a[k].push(v)
+        } else {
+          a[k] = [v]
+        }
+      })
+      return a
+    }, this.columnarFormat)
+  },
+  addFilter: function(filterName, column, value) {
+    this.filtersApplied.push({
+      filterName: filterName,
+      column: column,
+      value: value
+    })
+  },
+  clearFilters: function() {
+    this.filtersApplied = []
+    this.filtered = []
+  },
+  paginateData: function* (n) {
+    var index = 0
+    while (index + n < this.filtered.length) {
+      yield this.filtered.slice(index, index + n)
+      index = index + n
+    }
+  }
+}
+
 var tabulate = function (data,columns) {
     var table = d3.select('body').append('table')
       var thead = table.append('thead')
@@ -25,11 +104,59 @@ var tabulate = function (data,columns) {
       .append('td')
         .text(function (d) { return d.value })
   
-    return table;
-  }
+  return table;
+}
   
-  d3.csv('list.csv', function (data) {
-    console.log(data);
-    var columns = ['Common Name','Species Code']
-    tabulate(data,columns)
+d3.csv('../static/data/list.csv', function (data) {
+  // console.log(data);
+  DataController.loadData(data)
+  var columns = ['Common Name','Species Code']
+  DataController.addFilter('textIncludes', 'Common Name', 'bunting')
+  DataController.applyFilters()
+  const firstSlice = DataController.paginateData(50).next().value
+  console.log(firstSlice)
+  tabulate(firstSlice, columns)
+})
+
+function searchableBirds() {
+  var filter, table, tr, td, i;
+  
+  filter = input.value.toLowerCase();
+  tr = table.getElementsByTagName("tr");
+  table = document.getElementsByTagName("table");
+
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+d3.select("#birdInput")
+  .on("keyup", function() {
+    var searched_data = data,
+      text = this.value.trim();
+
+    var searchResults = searched_data.map(function(r) {
+      var regex = new RegExp("^" + text + ".*", "i");
+      if (regex.test(r.title)) {
+        return regex.exec(r.title)[0]
+      }
+    })
+  })
+
+  searchResults = searchResults.filter(function(r) {
+    return r != undefined;
+  })
+
+  searched_data = searchResults.map(function(r) {
+    return data.filter(function(p){
+      return p.title.indexOf(r) != -1;
+    })
   })
